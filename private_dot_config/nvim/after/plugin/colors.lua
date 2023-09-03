8fea4bce1ef1bb723adf6d05aa21b7ba6729ae54
Minura Molligoda <github@mournfully.net>
2023-09-02 20:32:45 -0400

    Set transparency for split panes
    https://www.reddit.com/r/neovim/comments/xwzd0z/how_to_make_neovim_splits_transparent/

--- private_dot_config/nvim/after/plugin/colors.lua
+++ private_dot_config/nvim/after/plugin/colors.lua
@@ -5,6 +5,9 @@ function SetColourScheme(color)
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "Normal", { fg = "none" })
 
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { fg = "none" })
+
 end
 
 SetColourScheme()
