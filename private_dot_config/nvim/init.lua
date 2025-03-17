6d17cb209713b268fccfde5a15ee7ced7b61b065
Minura Molligoda <github@mournfully.net>
2025-03-16 21:52:08 -0400

    Delete neovim plugin manager packer in favour of lazy

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,3 +1,8 @@
 -- 0 to LSP : Neovim RC From Scratch - YouTube - https://www.youtube.com/watch?v=w7i4amO_zaE
+
+-- GitHub - ThePrimeagen/init.lua - https://github.com/ThePrimeagen/init.lua
+-- 🚀 Getting Started | lazy.nvim - https://lazy.folke.io/
+
+require("mournfully.set")
 require("mournfully.remap")
 require("mournfully.set")
